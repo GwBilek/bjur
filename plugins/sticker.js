ae2aa4c54eed9071328c5fcabcf608ffa198a308
Dino A.F <92778687+DinnPsatirzZ@users.noreply.github.com>
2022-07-01 17:11:04 +0700

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -20,10 +20,10 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         stiker = await sticker(false, out, global.packname, global.author)
       } catch (e) {
         console.error(e)
-        if (!stiker) stiker = await sticker(img, false, global.packname, global.author)
+        if (!stiker) stiker = await sticker(img, false, "Sticker", "Bot")
       }
     } else if (args[0]) {
-      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], "Sticker", "Bot")
       else return m.reply('URL tidak valid!')
     }
   } catch (e) {
